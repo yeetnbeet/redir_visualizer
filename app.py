fed5c36fddff074bf37b5ac90571009360a37ac9
Ryan Littlefield <segalcsam@gmail.com>
2022-06-29 17:27:44 -0600

    Algorythm finds center parent

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ def csvIN () :
         output.append((line[0],line[1]))    
     graph = nx.Graph(output)
     return graph
-    
+
+#creates an interactive visual using pyvis.network    
 def visualize (graph) :
     net = Network(notebook=True)
     dir_network = nx.Graph(graph)
     net.from_nx(dir_network)
     net.show("output.html")
 
+#finds a group that could technically be a chain
 def getParentNodes (graph) :
     parentNodes = []
     for n in graph.nodes :
@@ -29,6 +31,7 @@ def getParentNodes (graph) :
             parentNodes.append(n)
     return parentNodes
 
+#finds a group that has more than one parent node
 def findOffender(graph,parentNodes) :
     badClusters = []
     for parentNode in parentNodes:
@@ -37,15 +40,29 @@ def findOffender(graph,parentNodes) :
             size = list(graph.neighbors(i))
             if (len(size) > 1):
                 badClusters.append(i)
+
+    for item in badClusters:
+        duplicates = list(nx.all_neighbors(graph,item))
+        for d in duplicates:
+            if (d in badClusters):
+                dlen = len(list(nx.all_neighbors(graph,d)))
+                itemlen = len(list(nx.all_neighbors(graph,item)))
+                if (dlen >= itemlen):
+                    badClusters.remove(item)
+                else:    
+                    badClusters.remove(d)
+        
             
-    print(badClusters)            
+    for item in badClusters:
+        print(item)
+    print(len(badClusters))            
 
         
 def main():
     net = Network(notebook=True)
     graph = csvIN()
-    visualize(graph)
     parentnodes = getParentNodes(graph)
     findOffender(graph,parentnodes)
+    visualize(graph)
      
 main()
